10eec6dc31ccc00e1059aef1e1bd5df0f1c34003
Charles Spencer Blancas <csgblancas@gmail.com>
2022-01-16 10:58:18 -0500

    Added win and lose functions

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
+let choices = ["Rock", "Paper", "Scissors"];
+
 function computerPlay(){
-    let choices = ["Rock", "Paper", "Scissors"];
     let picked = Math.floor(choices.length * Math.random());
     return choices[picked];
 }
 
 function round(playerSelection, computerSelection) {
     playerSelection = toSentenceCase(playerSelection);
-    
+    let playerIndex = choices.indexOf(playerSelection);
+    let computerIndex = choices.indexOf(computerSelection);
+
+    if (playerIndex == choices.length && computerIndex == 0) {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+    }
+    else if (computerIndex == choices.length && playerIndex == 0) {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`
+    }
 }
 
 function toSentenceCase(str) {
     return str.slice(0,1).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+function lose(playerSelection, computerSelection) {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+}
+
+function win(console.log(`You win! ${playerSelection} beats ${computerSelection}`)
