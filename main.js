4c0d87b9c4d660b2a5aad3cf64a0a163199ae00c
Charles Spencer Blancas <csgblancas@gmail.com>
2022-01-16 21:22:17 -0500

    Formatted

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let choices = ["Rock", "Paper", "Scissors"];
+let choices = ['Rock', 'Paper', 'Scissors'];
 
 game();
 
@@ -6,26 +6,23 @@ function game() {
     let playerScore = 0;
     let computerScore = 0;
     for (let i = 0; i < 5; i++) {
-        console.log(`Player: ${playerScore} Computer: ${computerScore}`)
-        let result = round(prompt("Rock, Paper, or Scissors?"), computerPlay());
+        console.log(`Player: ${playerScore} Computer: ${computerScore}`);
+        let result = round(prompt('Rock, Paper, or Scissors?'), computerPlay());
         console.log(result);
-        if (result.includes("lose")) {
-            computerScore++
-        }
-        else if (result.includes("win")) {
-            playerScore++
+        if (result.includes('lose')) {
+            computerScore++;
+        } else if (result.includes('win')) {
+            playerScore++;
         }
     }
     if (playerScore > computerScore) {
         console.log(`You won ${playerScore} to ${computerScore}`);
-    }
-    else if (playerScore < computerScore) {
+    } else if (playerScore < computerScore) {
         console.log(`You lost ${computerScore} to ${playerScore}`);
-    }
-    else console.log(`Draw. You both scored ${playerScore}`);
+    } else console.log(`Draw. You both scored ${playerScore}`);
 }
 
-function computerPlay(){
+function computerPlay() {
     let picked = Math.floor(choices.length * Math.random());
     return choices[picked];
 }
@@ -36,31 +33,26 @@ function round(playerSelection, computerSelection) {
     let computerIndex = choices.indexOf(computerSelection);
 
     if (playerIndex == -1) {
-        console.log("Unknown entry.");
-    }
-    else if (playerIndex == choices.length-1 && computerIndex == 0) {
+        console.log('Unknown entry.');
+    } else if (playerIndex == choices.length - 1 && computerIndex == 0) {
         return lose(playerSelection, computerSelection);
-    }
-    else if (computerIndex == choices.length-1 && playerIndex == 0) {
+    } else if (computerIndex == choices.length - 1 && playerIndex == 0) {
         return win(playerSelection, computerSelection);
-    }
-    else if (playerIndex > computerIndex) {
+    } else if (playerIndex > computerIndex) {
         return win(playerSelection, computerSelection);
-    }
-    else if (playerIndex < computerIndex) {
+    } else if (playerIndex < computerIndex) {
         return lose(playerSelection, computerSelection);
-    }
-    else return `${playerSelection} and ${computerSelection} draws.`
+    } else return `${playerSelection} and ${computerSelection} draws.`;
 }
 
 function toSentenceCase(str) {
-    return str.slice(0,1).toUpperCase() + str.slice(1).toLowerCase();
+    return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
 }
 
 function lose(playerSelection, computerSelection) {
-    return `You lose! ${computerSelection} beats ${playerSelection}`
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
 }
 
 function win(playerSelection, computerSelection) {
-    return `You win! ${playerSelection} beats ${computerSelection}`
+    return `You win! ${playerSelection} beats ${computerSelection}`;
 }
