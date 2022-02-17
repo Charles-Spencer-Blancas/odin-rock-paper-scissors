c575940876b9f54dba1369ffd9051091a5f4cb0e
Charles Spencer Blancas <charles.blancas@mail.mcgill.ca>
2022-02-16 23:44:36 -0500

    The site now displays the result of the round on the page

--- main.js
+++ main.js
@@ -8,46 +8,29 @@ selection.forEach((button) => {
     });
 });
 
-//game();
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 99999999; i++) {
-        console.log(`Player: ${playerScore} Computer: ${computerScore}`);
-        let result = round(prompt('Rock, Paper, or Scissors?'), computerPlay());
-        console.log(result);
-        if (result.includes('lose')) {
-            computerScore++;
-        } else if (result.includes('win')) {
-            playerScore++;
-        }
-    }
-    if (playerScore > computerScore) {
-        console.log(`You won ${playerScore} to ${computerScore}`);
-    } else if (playerScore < computerScore) {
-        console.log(`You lost ${computerScore} to ${playerScore}`);
-    } else console.log(`Draw. You both scored ${playerScore}`);
-}
-
 function computerPlay() {
     return Math.floor(choices.length * Math.random());
 }
 
 function round(playerIndex, computerIndex) {
-    console.log(playerIndex);
     playerSelection = choices[playerIndex];
     computerSelection = choices[computerIndex];
 
+    let output;
+
     if (playerIndex == choices.length - 1 && computerIndex == 0) {
-        return lose(playerSelection, computerSelection);
+        output = lose(playerSelection, computerSelection);
     } else if (computerIndex == choices.length - 1 && playerIndex == 0) {
-        return win(playerSelection, computerSelection);
+        output = win(playerSelection, computerSelection);
     } else if (playerIndex > computerIndex) {
-        return win(playerSelection, computerSelection);
+        output = win(playerSelection, computerSelection);
     } else if (playerIndex < computerIndex) {
-        return lose(playerSelection, computerSelection);
-    } else return `${playerSelection} and ${computerSelection} draws.`;
+        output = lose(playerSelection, computerSelection);
+    } else output = `${playerSelection} and ${computerSelection} draws.`;
+
+    let displayText = document.createElement('div');
+    displayText.textContent = output;
+    document.querySelector('.container').append(text);
 }
 
 function lose(playerSelection, computerSelection) {
